ae4fbcae6d27066c18fdc07bff1e85d44d161c1a
ninhnam <ninhnam027@gmail.com>
2024-04-19 22:49:19 +0700

    create new account when create new user

--- wallet-doan-chuyennganh-backend/controllers/auth.controller.js
+++ wallet-doan-chuyennganh-backend/controllers/auth.controller.js
@@ -3,6 +3,8 @@ import { sign } from "../utils/jwt.js";
 import bcrypt from 'bcrypt';
 import { UniqueConstraintError } from 'sequelize';
 import { generateUniqueHash, getTimestampPlus30Minutes, sendEmail } from "../utils/send-email.js";
+import { Account } from '../models/Account.js'
+import { ethers } from 'ethers'
 
 
 export const login = async (req, res) => {
@@ -57,7 +59,27 @@ export const signin = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = {email: email, password: hashedPassword};
 
-        await User.create(newUser);
+        const userModel = await User.create(newUser);
+
+        // add account start
+
+        const wallet = ethers.Wallet.createRandom();
+        const address = wallet.address;
+        const privateKey = wallet.privateKey;
+
+        let new_account = {
+            name: "Account " + address.slice(2, 8),
+            privateKey: privateKey,
+            address: address,
+            user_id: userModel.id
+        }
+
+        console.log("new_account ", new_account);
+
+        let created_account = await Account.create(new_account);
+        
+        // add account end
+        
         return res.status(201).json({message: "User created"});
     } catch (error) {
         if (error instanceof UniqueConstraintError && error.errors[0].path === 'email') {
@@ -104,15 +126,16 @@ export const forgotPassword = async (req, res) => {
 
 export const changePassword  = async (req, res) => {
     try {
-        let {email, password, newPassword} = req.body;
+        let {newPassword} = req.body;
+        let userRequest = req.user
 
-        if (!email || !password || !newPassword) {
-            return res.status(400).json({ error: "BadRequest: Email, password and newPassword are required" });
+        if (!newPassword) {
+            return res.status(400).json({ error: "BadRequest: newPassword are required" });
         }
 
         const user = await User.findOne({
             where: {
-                email: email
+                id: userRequest.id
             }
         });
 
@@ -120,27 +143,16 @@ export const changePassword  = async (req, res) => {
             return res.status(400).json({ error: "BadRequest: Not find user with email = " + email });
         }
 
-        const isPasswordMatch = await bcrypt.compare(password, user.password);
-        const isTempPasswordMatch = user.temporary_password == password && user.temporary_password_expired > Date.now();
-        if (isPasswordMatch || isTempPasswordMatch) {
-            if (newPassword.length < 6) {
-                return res.status(400).json({ error: "BadRequest: New password must be at least 6 characters long" });
-            } 
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        user.password = hashedPassword;
+        console.log("bf save", user);
 
-            const hashedPassword = await bcrypt.hash(newPassword, 10);
-            user.password = hashedPassword;
-            console.log("bf save", user);
+        await user.save();
+        console.log("save");
 
-            await user.save();
-            console.log("save");
-
-            return res.status(200).json({
-                message: "Change password successfully"
-            })
-        } else {
-            return res.status(400).json({ error: "BadRequest: Password not correct for email = " + email });
-        }
-        
+        return res.status(200).json({
+            message: "Change password successfully"
+        })
     } catch (error) {
         return res.status(400).json({ error: "BadRequest: Cannot change password"});
     }
--- wallet-doan-chuyennganh-backend/routes/auth.routes.js
+++ wallet-doan-chuyennganh-backend/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { forgotPassword, login, signin, changePassword } from "../controllers/auth.controller.js";
+import { authenticateToken } from "../utils/jwt.js";
 
 const router = Router();
 
 router.post("/login", login);
 router.post("/signup", signin);
 router.post("/forgot-password", forgotPassword)
-router.post("/change-password", changePassword)
+router.post("/change-password", authenticateToken, changePassword)
 
 export default router;
